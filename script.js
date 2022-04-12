e59863f6b3054ae4453e1d791f4c3844abce5ebb
Glitch (glitch-hello-website) <none>
2022-04-12 21:23:25 +0000

    💏🚉 Checkpoint ./script.js:37904035/2

--- script.js
+++ script.js
@@ -2,9 +2,9 @@ const numberButtons = document.querySelectorAll("[data-number]");
 const operationButtons = document.querySelectorAll("[data-operation]");
 const equalsButton = document.querySelector("[data-equals]");
 const allClearButton = document.querySelector("[data-all-clear]");
-
 const previousOperandTextElement = document.querySelector("[data-previous-operand]");
 const currentOperandTextElement = document.querySelector("[data-current-operand]");
+
 
 class Calculator {
   constructor(previousOperandTextElement, currentOperandTextElement) {
